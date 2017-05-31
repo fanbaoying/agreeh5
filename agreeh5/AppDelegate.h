4b6812d8196f52200bca4a8bbe4ce8bba61d4e06
ZhanFei <1479162730@qq.com>
2017-05-31 17:50:51 +0800

    Initial Commit

---
+++ agreeh5/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  agreeh5
+//
+//  Created by 范保莹 on 2017/5/31.
+//  Copyright © 2017年 agreePay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
